3c8828d0c8924b0c108901b6f076be85089023a5
kavindu-sayuranga <ksayuranga00@gmai.com>
2022-10-29 01:18:07 +0530

    Emitting custom events

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1> {{title}}</h1>
+<p>Welcome...</p>
+<!-- <input type="text" ref="name">
+<button @click="handleClick">click me</button> -->
+<div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale"/>
+
+</div>
+
+<button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from './components/Modal.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{Modal},
+  data(){
+    return{
+      title: 'My First Vue App :)',
+      header:'Sign up for giveaway',
+      text: 'grab your ninja swag for half price',
+      showModal: false
+    }
+  },
+
+  methods:{
+  //   handleClick(){
+  //     console.log(this.$refs.name)
+  //     this.$refs.name.classList.add('active')
+  //     this.$refs.name.focus()
+  //   }
+
+  toggleModal(){
+    this.showModal = !this.showModal
+  }
   }
 }
 </script>
@@ -22,5 +47,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1{
+  border-bottom: 1px solid blue;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{sale: theme==='sale'}">
+      <h1>{{header}}</h1>
+      <p>{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    props: ['header','text','theme']
+  }
+</script>
+
+<style scoped>
+.modal{
+  width: 400px;
+  padding: 20px;
+  margin: 200px auto;
+  background-color: white;
+  border-radius: 10px;
+}
+
+.backdrop{
+  top: 0;
+  position: fixed;
+  background: rgb(133, 131, 131);
+  width: 100%;
+  height: 100%;
+}
+h1{
+  color: aqua;
+  border: none;
+  padding: 0;
+}
+.modal.sale{
+  background: red;
+  color: white;
+}
+
+.modal.sale h1{
+  color: white;
+}
+</style>
